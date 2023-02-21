084244da2873907f74ce407dd94ab7b004821dc7
Behzod <behzodtuit@gmail.com>
2023-02-21 18:18:02 +0500

    deploy

--- students/admin.py
+++ students/admin.py
@@ -7,15 +7,6 @@ class StudentAdmin(admin.ModelAdmin):
     list_filter = ['added']
     search_fields =  ['name','phone','parent','birth']
     list_per_page = 10
-    def has_change_permission(self, request, obj=None):
-        if obj is not None and obj.user != request.user and obj.user.is_superuser:
-            return False
-        return True
-
-    def has_delete_permission(self, request, obj=None):
-        if obj is not None and obj.user != request.user and obj.user.is_superuser:
-            return False
-        return True
     def save_model(self, request, obj, form, change):
         print(obj.user.role)
         if obj.user.role == 'MANAGER' or obj.user.role=='DIRECTOR':
